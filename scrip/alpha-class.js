be7241ecf109e635100d2866d6408dc120adc46a
airabiul <rabiulislam074@gmail.com>
2024-02-15 11:46:35 +0600

    section desing is ok

---
+++ scrip/alpha-class.js
@@ -0,0 +1,10 @@
+
+function play(){
+    const homeScreen = document.getElementById("home-screen");
+    homeScreen.classList.add("hidden");
+    // console.log(homeScreen.classList);
+
+    const playGroundSection = document.getElementById("play-ground");
+    playGroundSection.classList.remove("hidden");
+    // console.log(playGroundSection.classList);
+}
